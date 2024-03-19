d3bcae46c44eabe57ba971d5e47e09b701d2e64a
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-19 21:25:22 +0400

    IoC -> beans added to container

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -1,13 +1,25 @@
 package com.cydeo.bean_annotation;
 
+import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.config.ComputerConfig;
+import com.cydeo.bean_annotation.config.RandomConfig;
+import com.cydeo.bean_annotation.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ComputerTest {
     public static void main(String[] args) {
 
-        ApplicationContext container = new AnnotationConfigApplicationContext();
+        ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class, RandomConfig.class);
 
+        Monitor monitor = container.getBean(Monitor.class);
+        Motherboard motherboard = container.getBean(Motherboard.class);
+        Case theCase = container.getBean(Case.class);
+
+        PC myPC = new PC(theCase,monitor,motherboard);
+
+        myPC.powerUp();
 
 
     }
