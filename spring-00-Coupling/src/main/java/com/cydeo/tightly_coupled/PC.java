9ef2b7c755312d3737a2ffc845b09a625ce0774f
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-19 18:37:43 +0400

    Coupling -> tightly coupled example

---
+++ spring-00-Coupling/src/main/java/com/cydeo/tightly_coupled/PC.java
@@ -0,0 +1,25 @@
+package com.cydeo.tightly_coupled;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class PC {
+
+    private DellCase theCase;
+    private AcerMonitor monitor;
+    private AsusMotherboard motherBoard;
+
+    public void powerUp(){
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo(){
+        monitor.drawPixelAt(1200, 50, "Orange");
+    }
+
+}
