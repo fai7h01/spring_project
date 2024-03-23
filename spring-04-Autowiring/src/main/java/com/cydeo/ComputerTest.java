7b34fb21df9861ac79b5f76273dc02d4b06ef80f
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-23 16:29:28 +0400

    Dependency injection

--- spring-04-Autowiring/src/main/java/com/cydeo/ComputerTest.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/ComputerTest.java
@@ -1,4 +1,16 @@
 package com.cydeo;
 
+import com.cydeo.config.PcConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ComputerTest {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(PcConfig.class);
+
+        PC pc = container.getBean(PC.class);
+        System.out.println(pc.getTheCase().getDimensions().getDepth());
+
+    }
 }
--- spring-04-Autowiring/src/main/java/com/cydeo/PC.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/PC.java
@@ -4,15 +4,17 @@ import com.cydeo.casefactory.Case;
 import com.cydeo.monitorfactory.Monitor;
 import com.cydeo.motherboardfactory.Motherboard;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Getter
+@Component
 public class PC {
 
     private Case theCase;
     private Monitor monitor;
     private Motherboard motherboard;
 
-    
     public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
         this.theCase = theCase;
         this.monitor = monitor;
