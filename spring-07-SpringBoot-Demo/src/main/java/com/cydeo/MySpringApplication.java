5644cd78703dc4e50f87c9651aaf262b6773175a
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-25 19:45:47 +0400

    Spring boot

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/MySpringApplication.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MySpringApplication {
+
+	public static void main(String[] args) {
+
+		Comment comment = new Comment();
+		comment.setAuthor("Luka");
+		comment.setText("Spring Framework");
+
+		ApplicationContext container = SpringApplication.run(MySpringApplication.class, args);
+
+		CommentService commentService = container.getBean(CommentService.class);
+		commentService.publishComment(comment);
+
+
+	}
+
+
+}
