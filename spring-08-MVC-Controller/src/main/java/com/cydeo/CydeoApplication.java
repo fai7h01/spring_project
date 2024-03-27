3bc1dc1d3467f6dcdba729e050e6173c0c94e2b6
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-27 18:10:46 +0400

    refactor

--- spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
@@ -1,4 +1,4 @@
-package com.example.spring08mvccontroller;
+package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
