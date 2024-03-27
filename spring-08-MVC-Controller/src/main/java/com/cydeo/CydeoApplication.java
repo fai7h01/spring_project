01ae89093af486517d2491dae45967188953b1bf
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-27 19:22:06 +0400

    MVC Controller

--- spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/CydeoApplication.java
@@ -2,7 +2,9 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 
+//@SpringBootApplication(exclude = {AopAutoConfiguration.class})
 @SpringBootApplication
 public class CydeoApplication {
 
