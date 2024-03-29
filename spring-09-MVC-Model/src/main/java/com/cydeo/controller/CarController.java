05e63b016ae6df86f88de5954f9af3be6cb8c975
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-30 00:06:37 +0400

    MVC

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,26 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CarController {
+
+    @RequestMapping("/info")
+    public String carInfo(@RequestParam String make, Model model){
+
+        model.addAttribute("make",make);
+
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info2")
+    public String carInfo2(@RequestParam(value = "make",required = false,defaultValue = "KIA") String make, Model model){
+
+        model.addAttribute("make",make);
+
+        return "car/car-info";
+    }
+}
