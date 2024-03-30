afe9d3bed607890d6849bb33c6847cea38a800ba
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-30 23:27:22 +0400

    Thymeleaf

---
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,34 @@
+package com.cydeo.controller;
+
+import com.cydeo.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("/register")  //localhost:8080/student/register
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+    }
+
+    @RequestMapping("/drop")  //localhost:8080/student/drop
+    public String drop(){
+
+
+        return "student/register";
+    }
+
+    @RequestMapping("/welcome")  //localhost:8080/student/welcome?name=Luka
+    public String welcome(@RequestParam String name){
+
+        System.out.println(name);
+        return "student/welcome";
+    }
+}
