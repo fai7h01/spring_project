98db416e7d07ac07031a5be4f1c326df265cec96
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-05 16:17:27 +0400

    register form

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,18 +4,38 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
     @GetMapping("/register") //localhost:8080/mentor/register
-    public String register(Model model){
+    public String register(Model model) {
 
+        List<String> batchList = Arrays.asList("JD1", "JD2", "JD3", "JD4");
+
+        model.addAttribute("batchList", batchList);
         model.addAttribute("mentor", new Mentor());
 
         return "mentor/mentor-register";
     }
 
+    @PostMapping("/confirm")
+    public String submitForm() {
+
+
+        return "mentor/mentor-confirmation";
+    }
+
+    @GetMapping("/confirm")
+    public String submitForm2() {
+
+
+        return "mentor/mentor-confirmation";
+    }
 }
