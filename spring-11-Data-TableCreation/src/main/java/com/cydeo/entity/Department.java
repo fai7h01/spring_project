7a8e1dda2df0a57f29923a8f38883148d39d5869
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-04 01:19:48 +0400

    Spring Data

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Department.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Department.java
@@ -2,9 +2,13 @@ package com.cydeo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department extends BaseEntity{
 
     private String department;
