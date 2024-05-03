ab5a31ca4c575a98c7fa6b4c48dbc24965630c91
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-03 17:42:55 +0400

    Spring Data

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into EMPLOYEES(id,name)
 values (1,'Mike');
+
+insert into student(id,first_name,last_name,email)
+values (1,'Luka','Faith','lv@gmail.com');
+
+insert into student(id,first_name,last_name,email)
+values (2,'Roger','Penrose','rp@gmail.com');
