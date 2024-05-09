43453d74b5ac1b9c64fcec94fde2b70d53d31f8b
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-09 20:36:54 +0400

    table creation

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
@@ -0,0 +1,24 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Ticket extends BaseEntity{
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime dateTime;
+    private Integer seatNumber;
+    private Integer rowNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MovieCinema movieCinema;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User userAccount;
+
+}
