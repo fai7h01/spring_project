5f2b60e3c442f44ab71a50241160922adcbb8752
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-11 19:41:13 +0400

    entity creation

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    private Long id;
+
+}
