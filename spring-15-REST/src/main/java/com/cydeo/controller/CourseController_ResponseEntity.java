7738481d69cbcc57c719aa090eaf23ccc5c9bf94
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-20 17:53:08 +0400

    Spring REST

---
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -0,0 +1,9 @@
+package com.cydeo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses/api/v2")
+public class CourseController_ResponseEntity {
+}
