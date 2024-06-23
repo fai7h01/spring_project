38a36666d6fc0b28338f0173ec5ec0bc5d5ea7f0
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-23 21:19:11 +0400

    Spring REST - Consuming Apis -> Web Client

--- spring-17-REST-ConsumingAPI/src/main/java/com/cydeo/Spring17RestConsumingApiApplication.java
+++ spring-17-REST-ConsumingAPI/src/main/java/com/cydeo/Spring17RestConsumingApiApplication.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -19,4 +20,8 @@ public class Spring17RestConsumingApiApplication {
         return new RestTemplate();
     }
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
