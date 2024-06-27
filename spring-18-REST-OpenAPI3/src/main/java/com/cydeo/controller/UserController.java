f3183763d7d8fff68d3bc1b4fabdccacb10e3744
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-27 15:53:52 +0400

    Spring REST - OpenAPI3 -> User Controller, devTools added

--- spring-18-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
+++ spring-18-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Tag(name = "User", description = "User CRUD Operations")
 public class UserController {
 
     private final UserService userService;
@@ -19,11 +22,13 @@ public class UserController {
     }
 
     @GetMapping("/list")
+    @Operation(summary = "Read All Users")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getUsers());
     }
 
     @PostMapping
+    @Operation(summary = "Create a User")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(userDTO));
     }
