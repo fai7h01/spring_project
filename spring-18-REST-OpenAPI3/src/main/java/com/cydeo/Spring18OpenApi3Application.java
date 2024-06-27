7fcb79582726578800bd3a1df9ac0411101f4d9f
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-27 15:43:53 +0400

    Spring REST - OpenAPI3

--- spring-18-REST-OpenAPI3/src/main/java/com/cydeo/Spring18OpenApi3Application.java
+++ spring-18-REST-OpenAPI3/src/main/java/com/cydeo/Spring18OpenApi3Application.java
@@ -1,9 +1,14 @@
 package com.cydeo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Spring18OpenApi3Application {
@@ -15,6 +20,17 @@ public class Spring18OpenApi3Application {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info()
+                .title("Practice Application OpenAPI")
+                .version("v1")
+                .description("Practice Application API Documentation"))
+                .servers(List.of(
+                        new Server().url("https://localhost:8080").description("Practice Environment")
+                ));
     }
 
 }
