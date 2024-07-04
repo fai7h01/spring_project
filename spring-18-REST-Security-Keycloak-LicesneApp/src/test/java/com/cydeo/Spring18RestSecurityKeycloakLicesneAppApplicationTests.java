b75941f4d33dd4d727e16647dfb05d569d803537
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-04 14:52:30 +0400

    Spring REST security

---
+++ spring-18-REST-Security-Keycloak-LicesneApp/src/test/java/com/cydeo/Spring18RestSecurityKeycloakLicesneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring18RestSecurityKeycloakLicesneAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
