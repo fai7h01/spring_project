cd38f9b4394d79f291dd3f3fd15a1328571137f7
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-16 19:01:29 +0400

    Unit Testing & JUnit 5

---
+++ spring-21-UnitTesting-JUnit5/src/test/java/com/cydeo/Spring21UnitTestingJUnit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring21UnitTestingJUnit5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
