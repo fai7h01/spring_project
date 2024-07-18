54d20813c10a1d1e224bd7d04fbcf8d8296f504c
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-18 17:13:23 +0400

    Unit Testing & JUnit 5

--- spring-21-UnitTesting-JUnit5/src/test/java/com/cydeo/CalculatorParameterizedTest.java
+++ spring-21-UnitTesting-JUnit5/src/test/java/com/cydeo/CalculatorParameterizedTest.java
@@ -1,8 +1,7 @@
 package com.cydeo;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,4 +19,22 @@ class CalculatorParameterizedTest {
         assertEquals(0, num % 3);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "JS", "TS"})
+    //@EmptySource
+    //@NullSource
+    @NullAndEmptySource
+    void testCase3(String arg){
+        assertFalse(arg.isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringProvider")
+    void testCase4(String arg){
+        assertFalse(arg.isEmpty());
+    }
+
+    static String[] stringProvider(){
+        return new String[]{"Java", "JS", "TS"};
+    }
 }
