ec1dffb59f6d19e6d98dfe0fb5dc848db5faf5ab
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-18 17:38:38 +0400

    Unit Testing & JUnit 5

--- spring-21-UnitTesting-JUnit5/src/test/java/com/cydeo/CalculatorParameterizedTest.java
+++ spring-21-UnitTesting-JUnit5/src/test/java/com/cydeo/CalculatorParameterizedTest.java
@@ -9,13 +9,13 @@ class CalculatorParameterizedTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"Java", "JS", "TS"})
-    void testCase1(String arg){
+    void testCase1(String arg) {
         assertFalse(arg.isEmpty());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {9, 36, 12})
-    void testCase2(int num){
+    void testCase2(int num) {
         assertEquals(0, num % 3);
     }
 
@@ -24,17 +24,33 @@ class CalculatorParameterizedTest {
     //@EmptySource
     //@NullSource
     @NullAndEmptySource
-    void testCase3(String arg){
+    void testCase3(String arg) {
         assertFalse(arg.isEmpty());
     }
 
     @ParameterizedTest
     @MethodSource("stringProvider")
-    void testCase4(String arg){
+    void testCase4(String arg) {
         assertFalse(arg.isEmpty());
     }
 
-    static String[] stringProvider(){
+    static String[] stringProvider() {
         return new String[]{"Java", "JS", "TS"};
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10, 20, 30",
+            "20, 30, 50",
+            "100, 200, 400"
+    })
+    void testCase5(int num1, int num2, int result) {
+        assertEquals(result, Calculator.add(num1, num2));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sample-data.csv")
+    void testCase6(int num1, int num2, int result) {
+        assertEquals(result, Calculator.add(num1, num2));
     }
 }
