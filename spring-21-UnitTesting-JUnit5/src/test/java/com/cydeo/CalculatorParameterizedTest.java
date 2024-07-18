57d9c19a084c452a848fc5d3c69a2d3044cd1adb
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-18 17:41:24 +0400

    Unit Testing & JUnit 5

--- spring-21-UnitTesting-JUnit5/src/test/java/com/cydeo/CalculatorParameterizedTest.java
+++ spring-21-UnitTesting-JUnit5/src/test/java/com/cydeo/CalculatorParameterizedTest.java
@@ -49,7 +49,7 @@ class CalculatorParameterizedTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/sample-data.csv")
+    @CsvFileSource(resources = "/sample-data.csv", numLinesToSkip = 1)
     void testCase6(int num1, int num2, int result) {
         assertEquals(result, Calculator.add(num1, num2));
     }
