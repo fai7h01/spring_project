def38c35301b0db305ceca7ab97f0b58a6cc1182
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-19 16:29:13 +0400

    Unit Testing & Mockito

--- spring-22-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/TaskServiceImplTest.java
+++ spring-22-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/TaskServiceImplTest.java
@@ -15,8 +15,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceImplTest {
@@ -40,10 +42,29 @@ class TaskServiceImplTest {
         when(taskRepository.findById(id)).thenReturn(Optional.of(task));
         when(taskMapper.convertToDto(task)).thenReturn(new TaskDTO());
 
-        taskService.findById(id);
+        TaskDTO taskDTO = taskService.findById(id);
 
         verify(taskRepository).findById(id);
-        verify(taskMapper).convertToDto(task);
+        verify(taskMapper).convertToDto(task);;
+
+        assertNotNull(taskDTO);
+    }
+
+    @Test
+    void findById_BDD_Test(){
+
+        //given
+        Task task = new Task();
+        given(taskRepository.findById(anyLong())).willReturn(Optional.of(task));
+        given(taskMapper.convertToDto(task)).willReturn(new TaskDTO());
+
+        //when
+        taskService.findById(anyLong());
+
+        //then
+        then(taskRepository).should().findById(anyLong());
+        then(taskMapper).should(atLeastOnce()).convertToDto(task);
+
     }
 
 }
