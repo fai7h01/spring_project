15fd7da3613b1dc2ccfdf4d9ffe0ec01f23bb49e
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-19 15:42:50 +0400

    Unit Testing & Mockito

--- spring-22-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/ProjectServiceImplTest.java
+++ spring-22-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/ProjectServiceImplTest.java
@@ -11,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.NoSuchElementException;
+
 import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,17 +33,33 @@ class ProjectServiceImplTest {
     @Test
     void getByProjectCode_Test(){
 
+        //given
         when(projectRepository.findByProjectCode(anyString())).thenReturn(new Project()); // stubbing
         when(projectMapper.convertToDto(any(Project.class))).thenReturn(new ProjectDTO());
 
+        //when
         ProjectDTO projectDTO = projectService.getByProjectCode(anyString());
 
+        //then
         InOrder inOrder = inOrder(projectRepository, projectMapper);
 
         inOrder.verify(projectRepository).findByProjectCode(anyString());
         inOrder.verify(projectMapper).convertToDto(any(Project.class));
 
         assertNotNull(projectDTO);
+    }
+
+    @Test
+    void getByProjectCode_ExceptionTest(){
+
+        when(projectRepository.findByProjectCode("")).thenThrow(new NoSuchElementException("Project not found"));
+
+        Throwable throwable = assertThrows(NoSuchElementException.class, () -> projectService.getByProjectCode(""));
+
+        verify(projectRepository).findByProjectCode("");
+        verify(projectMapper, never()).convertToDto(any(Project.class));
+
+        assertEquals("Project not found", throwable.getMessage());
     }
 
 }
